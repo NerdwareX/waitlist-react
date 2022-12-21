cf09fccf574571cda426f4eac80a6c9e0e3567e8
Anita Ikediashi <104375245+AnitaIkediashi@users.noreply.github.com>
2022-12-21 13:22:46 +0100

    Modified navbar component

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
-  const [active, setActive] = useState(0)
+  const [active, setActive] = useState(0);
 
   const showAllLinks = () => setShowLinks(!showLinks);
 
@@ -12,8 +12,17 @@ const Navbar = () => {
     <>
       {/* desktop */}
       <header className="w-full py-[41px] flex items-center fixed top-0 2xl:px-[186px] lg:px-20 px-8 bg-[#000074] z-[150]">
-        <nav className="w-full h-full flex items-center justify-between">
+        <nav className="w-full h-full flex items-center sm:justify-between justify-around">
           {/* left */}
+          {/* hamburger-menu */}
+          <motion.div
+            className="text-white text-lg cursor-pointer xl:hidden"
+            onClick={showAllLinks}
+            whileTap={{ scale: 0.9 }}
+          >
+            <i className="ri-menu-fill"></i>
+          </motion.div>
+
           <div className="flex items-center sm:gap-1 gap-0">
             <img src={logo} alt="company logo" />
             <h1 className="font-Inter text-white text-base sm:text-2xl font-semibold">
@@ -69,21 +78,13 @@ const Navbar = () => {
               <i className="ri-shopping-cart-line"></i>
             </motion.li>
           </ul>
-          <div className="flex items-center gap-4 ">
-            <motion.button
-              className="px-[23px] py-2 btn-primary rounded-[20px] text-white hidden md:flex"
-              whileTap={{ scale: 0.9 }}
-            >
-              Connect wallet
-            </motion.button>
-            <motion.div
-              className="text-white text-lg cursor-pointer xl:hidden"
-              onClick={showAllLinks}
-              whileTap={{ scale: 0.9 }}
-            >
-              <i className="ri-menu-fill"></i>
-            </motion.div>
-          </div>
+
+          <motion.button
+            className="px-[23px] py-2 btn-primary rounded-[20px] text-white hidden sm:flex"
+            whileTap={{ scale: 0.9 }}
+          >
+            Connect wallet
+          </motion.button>
         </nav>
       </header>
 
@@ -92,10 +93,10 @@ const Navbar = () => {
         className={`fixed top-0 right-0 left-0 bottom-0 h-screen w-full z-[200] opacity-100 duration-300 ease-in bg-[rgba(0,0,0,0.3)] xl:hidden ${
           showLinks
             ? "opacity-100 visible translate-x-0"
-            : "opacity-0 invisible translate-x-8"
+            : "opacity-0 invisible -translate-x-8"
         }`}
       >
-        <div className="w-full h-screen bg-[#000074] max-w-xs absolute top-0 right-0 xl:hidden">
+        <div className="w-full h-screen bg-[#000074] max-w-xs absolute top-0 left-0 xl:hidden">
           <ul className=" w-full h-full flex items-center justify-center gap-6 flex-col">
             <li
               className={`text-white cursor-pointer ${
@@ -148,7 +149,7 @@ const Navbar = () => {
           </ul>
         </div>
         <motion.div
-          className="absolute top-6 right-6 cursor-pointer text-2xl text-white"
+          className="absolute top-6 left-6 cursor-pointer text-2xl text-white"
           onClick={showAllLinks}
           whileTap={{ rotate: 360 }}
         >
